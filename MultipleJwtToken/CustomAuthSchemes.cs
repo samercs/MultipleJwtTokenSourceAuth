0c5a9fa7ff2a049612ab98c0512b8cd4f8e6031a
Samer Hasan Al Khatib <samer.alkhatib@upda.gov.bh>
2024-11-18 13:28:55 +0300

    add supabase and keycloak

---
+++ MultipleJwtToken/CustomAuthSchemes.cs
@@ -0,0 +1,7 @@
+﻿namespace MultipleJwtToken;
+
+public static class CustomAuthSchemes
+{
+    public static string Keycloak = "Keycloak";
+    public static string Supabase = "Supabase";
+}
--- MultipleJwtToken/Program.cs
+++ MultipleJwtToken/Program.cs
@@ -3,13 +3,25 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.IdentityModel.Tokens;
+using MultipleJwtToken;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+    .AddJwtBearer(CustomAuthSchemes.Keycloak, i =>
+    {
+        i.RequireHttpsMetadata = false;
+        i.Audience = builder.Configuration["Auth:Keycloak:Audience"]!;
+        i.MetadataAddress = builder.Configuration["Auth:Keycloak:MetadataUrl"]!;
+        i.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = builder.Configuration["Auth:Keycloak:ValidIssuer"]!,
+        };
+    })
+    .AddJwtBearer(CustomAuthSchemes.Supabase, options =>
     {
         byte[] bytes = Encoding.UTF8.GetBytes(builder.Configuration["Auth:Supabase:JwtSecret"]!);
         options.TokenValidationParameters = new TokenValidationParameters()
@@ -19,6 +31,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidIssuer = builder.Configuration["Auth:Supabase:ValidIssuer"]
         };
     });
+
+builder.Services.AddAuthorization(option =>
+{
+    var defaultSchema = new AuthorizationPolicyBuilder(CustomAuthSchemes.Keycloak, CustomAuthSchemes.Supabase)
+        .RequireAuthenticatedUser()
+        .Build();
+    option.DefaultPolicy = defaultSchema;
+});
 builder.Services.AddMvc();
 builder.Services.Configure<JsonOptions>(i =>
 {
@@ -34,7 +54,7 @@ app.UseAuthorization();
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/me", async (ClaimsPrincipal cp) =>
 {
-    return Results.Ok(cp);
+    return Results.Ok(cp.Claims.ToDictionary(i=> i.Type, j=>j.Value));
 }).RequireAuthorization();
 
 app.Run();
